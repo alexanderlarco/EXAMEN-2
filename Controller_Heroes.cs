c876d61a5e66c4ada7b07acbc3ac5e28c07333a1
alexander larco <52249910+alexanderlarco@users.noreply.github.com>
2021-02-03 20:10:07 -0500

    Examen de programacion visual
    Examen final

---
+++ Controller_Heroes.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Data;
+using LarcoExamen.Models;
+
+namespace LarcoExamen.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Controller_SuperHeroes : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public Controller_SuperHeroes(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select * from dbo.SuperHeroes";
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("HeroTourAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myComand = new SqlCommand(query, myCon))
+                {
+                    myReader = myComand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            string query = @"select * from dbo.SuperHeroes
+                                where id = " + id + @"
+                                ";
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("HeroTourAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open(); ;
+                using (SqlCommand myComand = new SqlCommand(query, myCon))
+                {
+                    myReader = myComand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        
+        
+    }
+}
